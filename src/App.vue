a603c525c1caa3bcf8fbfead1fc70ca93e52b912
Angelos Moavinis <aggelos@istognosis.com>
2026-02-07 00:54:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="container">
+    <particles-bg type="cobweb" color="white" :bg="true" />
+    <Header @event1="selectTab"></Header>
+    <Information v-if="selectedTab==0"/>
+    <Demographics v-if="selectedTab==1"/>
+    <Topics v-if="selectedTab==2"/>
+    <Clustering v-if="selectedTab==3" />
+    <GraphAnalysis v-if="selectedTab==4"/>
+
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Demographics from './components/Demographics.vue'
+import Topics from './components/Topics.vue'
+import Clustering from './components/Clustering'
+import Information from './components/Information.vue'
+import GraphAnalysis from './components/GraphAnalysis.vue'
+import { ParticlesBg } from "particles-bg-vue";
+
+export default {
+  name: 'App',
+  components: {
+    ParticlesBg,
+    Header,
+    Demographics,
+    Topics,
+    Clustering,
+    Information,
+    GraphAnalysis,
+  },
+
+  data: () => {
+    return {
+      selectedTab: 0
+    };
+  },
+
+  methods: {
+    selectTab(t) {
+      this.selectedTab = t
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
